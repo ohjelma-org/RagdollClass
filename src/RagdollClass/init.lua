17c6adc104453f5e863f80f6337c390696ea3b89
aku <68713456+aku-e@users.noreply.github.com>
2020-11-01 05:53:08 +0200

    renamed method names, added license link and source link

--- src/RagdollClass/init.lua
+++ src/RagdollClass/init.lua
@@ -1,7 +1,7 @@
 --[[
 	Author: guidable
-	License:
-	Source:
+	License: https://github.com/ohjelma-org/RagdollClass/blob/main/LICENSE.md
+	Source: https://github.com/ohjelma-org/RagdollClass/blob/main/src/RagdollClass/init.lua
 --]]
 
 local RagdollClass = {}
@@ -18,9 +18,14 @@ local Maid = require(script.Maid)
 function RagdollClass.new(character: Model)
 	assert(RunService:IsServer(), "RagdollClass can only be used from the server")
 	local self = setmetatable({
-		character = character,
+		-- @@ Public Members
+		Enabled = false, -- read only
+		Character = character, -- read only
+
+		-- @@ Private Members
 		humanoidRootPart = nil,
 		humanoid = nil,
+
 		maid = Maid.new(),
 	}, RagdollClass)
 	self:setup()
@@ -29,34 +34,77 @@ end
 
 -- @@ Private Methods
 function RagdollClass:setup()
-	local humanoidRootPart = self.character:FindFirstChild("HumanoidRootPart")
-	local humanoid = self.character:FindFirstChildWhichIsA("Humanoid")
+	local humanoidRootPart = self.Character:FindFirstChild("HumanoidRootPart")
+	local humanoid = self.Character:FindFirstChildWhichIsA("Humanoid")
 
 	assert(humanoidRootPart, "character does not have a HumanoidRootPart")
 	assert(humanoid, "character does not have a Humanoid")
 
+	self.player = Players:GetPlayerFromCharacter(self.Character)
 	self.humanoidRootPart = humanoidRootPart
 	self.humanoid = humanoid
 end
 
 -- @@ Public Methods
-function RagdollClass:Start(pointOfContact: Vector3)
-	local player = Players
-	self.maid.ragdollFolder = Ragdoll.createRagdollConstraints(self.character, self.humanoid.RigType)
-	local motors = Ragdoll.disableMotors(self.character)
+function RagdollClass:Enable(pointOfContact: CFrame)
+	assert(not self.Enabled, "ragdoll is already enabled")
+	self.Enabled = true
+
+	local player = self.player
+	local character = self.Character
+	local humanoidRootPart = self.humanoidRootPart
+	local humanoid = self.humanoid
+
+	assert(humanoidRootPart:CanSetNetworkOwnership(), "must be able to set HumanoidRootPart network ownership auto")
+	local requiresNeck = humanoid.RequiresNeck
+	humanoid.RequiresNeck = false
+
+	local ragdollConstraints = Ragdoll.CreateRagdollConstraints(character, humanoid)
+	local motors = Ragdoll.DisableMotors(character)
+
+	local networkOwner = humanoidRootPart:GetNetworkOwner()
+	local networkOwnershipAuto = humanoidRootPart:GetNetworkOwnershipAuto()
+	if player then
+		humanoidRootPart:SetNetworkOwner(player)
+		Replication.Fire(player, true, character, motors, pointOfContact)
+	else
+		humanoidRootPart:SetNetworkOwner(nil)
+		Ragdoll.SetupHumanoid(humanoid, motors, pointOfContact)
+	end
 
 	self.maid:GiveTask(function()
-		Ragdoll.enableMotors(motors)	
+		Ragdoll.EnableMotors(motors)
+		Ragdoll.RemoveRagdollConstraints(ragdollConstraints)
+		if player then
+			Replication.Fire(player, false, character)
+		else
+			Ragdoll.ResetHumanoid(humanoid)
+		end
+
+		if humanoidRootPart:CanSetNetworkOwnership() then
+			humanoidRootPart:SetNetworkOwner(networkOwner)
+			if networkOwnershipAuto then
+				humanoidRootPart:SetNetworkOwnershipAuto()
+			end
+		end
+		humanoid.RequiresNeck = requiresNeck
 	end)
 end
 
-function RagdollClass:Stop()
-
+function RagdollClass:Disable()
+	assert(self.Enabled, "ragdoll is not enabled")
+	self.Enabled = false
+	self.maid:Destroy()
 end
 
 -- @@ Class Destructors
 function RagdollClass:Destroy()
+	self.maid:Destroy()
 
+	self.player = nil
+	self.Character = nil
+	self.humanoidRootPart = nil
+	self.humanoid = nil
 end
 
 return RagdollClass
